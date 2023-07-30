9521d9fd338b82e3d3f75fcf922b23acd0c2ae4a
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-07-30 03:26:54 -0600

    dicks and cheese

---
+++ src/pages/ProjectsPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <main class="container-fluid">
+        <section class="row">
+            <div class="col-6">
+                <div class="">
+                    <img class="compass-background" src="src/assets/img/cooking-background.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-6">
+                <div>
+                    <img src="src/assets/" alt="">
+                </div>
+            </div>
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </section>
+    </main>
+</template>
+
+
+<script>
+export default {
+    setup(){
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .compass-background {
+        height: 30rem;
+        object-fit: contain;
+    }
+</style>
