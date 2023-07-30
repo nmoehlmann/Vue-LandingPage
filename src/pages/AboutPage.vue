12c8a5f1d8d3c2c6efe23d7f4c6e8acca6f993f4
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-07-30 17:21:33 -0600

    porjcets page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -4,31 +4,32 @@
       <img class="about-img" src="https://placehold.co/600x400" alt="">
     </section>
     <section class="row mt-5">
-      <h1 class="header">Hello!</h1>
+      <h1 class="hello-header">Hello!</h1>
     </section>
     <section class="row">
-      <article class="col-6 text-light bio">
+      <article class="col-12 text-light bio">
         <p>
           I am a Software Developer with extensive experience in both front and backend. I love working on complex projects that require methodical planning and execution. Solving difficult problems is what Software Development is all about, and that's what I'm here for. Outside of coding, I enjoy playing piano, videogames, and working out.
         </p>
       </article>
-      <div class="col-6 text-light contact">
-        <div>
-          <p><span class="fw-bold">Age:</span> 20</p>
-          <p><span class="fw-bold">Education:</span> Boise CodeWorks</p>
-          <p><span class="fw-bold">Email:</span> nathan.moehlmann@gmail.com</p>
-          <p><span class="fw-bold">phone:</span> 208-697-7595</p>
-        </div>
-      </div>
     </section>
     <section class="row">
-      <p class="header">Coding Skills</p>
-      <div class="skills">
+      <div class="col-6 skills">
+        <p class="header">Knowledge</p>
         <li>Vue <i class="mdi mdi-vuejs"></i></li>
         <li>Node <i class="mdi mdi-nodejs"></i></li>
         <li>SQL <i class="mdi mdi-database-search"></i></li>
         <li>C# <i class="mdi mdi-dot-net"></i></li>
         <li>HTML <i class="mdi mdi-language-html5"></i> / CSS <i class="mdi mdi-language-css3"></i></li>
+      </div>
+      <div class="col-6 text-light contact">
+        <div>
+          <p class="header">Contact</p>
+          <p><span class="fw-bold">Age:</span> 20</p>
+          <p><span class="fw-bold">Education:</span> Boise CodeWorks</p>
+          <p><span class="fw-bold">Email:</span> nathan.moehlmann@gmail.com</p>
+          <p><span class="fw-bold">phone:</span> 208-697-7595</p>
+        </div>
       </div>
     </section>
   </main>
@@ -45,6 +46,11 @@ export default {
 </script>
 
 <style lang="scss">
+  .hello-header {
+    font-size: 5rem;
+    color: whitesmoke;
+    margin-bottom: 2rem;
+  }
   .about-img {
     height: 50rem;
     object-fit: contain;
@@ -52,14 +58,12 @@ export default {
 
   .header {
     color: whitesmoke;
-    font-size: 4rem;
+    font-size: 3rem;
     margin-bottom: 2rem;
   }
 
   .contact {
     display: flex;
-    justify-content: center;
-
     font-size: 1.5rem;
   }
 
@@ -69,6 +73,9 @@ export default {
     
     font-size: 1.4rem;
     line-height: 2.5rem;
+
+    border-bottom: solid var(--light-gray);
+    margin-bottom: 1rem;
   }
 
   .skills {
--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -2,13 +2,17 @@
     <main class="container-fluid">
         <section class="row">
             <div class="col-6">
-                <div class="">
-                    <img class="compass-background" src="src/assets/img/cooking-background.jpg" alt="">
+                <div class="compass-background">
+                    <div class="cooking-compass-container">
+                        <img class="cooking-compass" src="src/assets/img/cooking-compass.svg" alt="">
+                    </div>
                 </div>
             </div>
             <div class="col-6">
-                <div>
-                    <img src="src/assets/" alt="">
+                <div class="allSpice-background">
+                    <div class="allSpice-logo-container">
+                        <p class="allSpice-logo">All Spice</p>
+                    </div>
                 </div>
             </div>
             <div class="col-6"></div>
@@ -28,8 +32,58 @@ export default {
 
 
 <style lang="scss" scoped>
+
+    // All Spice
+    .allSpice-background {
+        background-image: url(src/assets/img/food-img.avif);
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 35rem;
+        aspect-ratio: 1/1;
+        border-radius: 10px;
+    }
+
+    .allSpice-logo {
+        font-family: 'Noto Serif', serif;
+        font-size: 7rem;
+        color: whitesmoke;
+        // background: rgba($color: #202020, $alpha: .7);
+        border-radius: 10px;
+        text-shadow: 2px 2px 3px black;
+    }
+
+    .allSpice-logo-container {
+        backdrop-filter: blur(2.5px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    // cooking compass
     .compass-background {
-        height: 30rem;
+        background-image: url(src/assets/img/cooking-background.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 35rem;
+        aspect-ratio: 1/1;
+        border-radius: 10px;
+    }
+
+    .cooking-compass {
         object-fit: contain;
+        height: 20rem;
+        background: rgba($color: #202020, $alpha: .7);
+        border-radius: 10px;
+    }
+
+    .cooking-compass-container {
+        backdrop-filter: blur(3px);
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 </style>
