f280077cd63581bf94406ca96178aec36b9fb4f9
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-07-30 20:07:04 -0600

    asdf

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,22 +1,32 @@
 <template>
     <main class="container-fluid">
         <section class="row">
-            <div class="col-6">
+            <div class="col-12 header">
+                <h1>Projects</h1>
+            </div>
+        </section>
+        <section class="row">
+            <div class="col-xxl-6 d-flex p-container">
                 <div class="compass-background">
                     <div class="cooking-compass-container">
                         <img class="cooking-compass" src="src/assets/img/cooking-compass.svg" alt="">
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-xxl-6 p-container">
                 <div class="allSpice-background">
                     <div class="allSpice-logo-container">
                         <p class="allSpice-logo">All Spice</p>
                     </div>
                 </div>
             </div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+            <div class="col-xxl-6 p-container">
+                <div class="keepr-background">
+                    <div class="keepr-logo-container">
+                        <img class="keepr-logo" src="src/assets/img/Keepr-logo.png" alt="">
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 </template>
@@ -32,6 +42,38 @@ export default {
 
 
 <style lang="scss" scoped>
+
+    .header {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        color: white
+    }
+
+    // Keepr
+
+    .keepr-background {
+        background-image: url(src/assets/img/masonry.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 35rem;
+        aspect-ratio: 1/1;
+        border-radius: 10px;
+    }
+
+    .keepr-logo {
+        background-color: white;
+        height: 15rem;
+        border-radius: 8%;
+    }
+
+    .keepr-logo-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        backdrop-filter: blur(2px);
+    }
 
     // All Spice
     .allSpice-background {
@@ -85,5 +127,11 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .p-container {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 1.5rem;
     }
 </style>
