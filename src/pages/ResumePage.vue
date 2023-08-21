41c28920666b1feb1cc34c54528f9709d1123769
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-08-20 19:25:29 -0600

    updated resume

--- src/pages/ResumePage.vue
+++ src/pages/ResumePage.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <iframe class="resume vh-100" src="/MyResume.pdf" frameborder="0"></iframe>
+        <iframe class="resume vh-100" src="/Nathan'sResume.pdf" frameborder="0"></iframe>
     </main>
 </template>
 
